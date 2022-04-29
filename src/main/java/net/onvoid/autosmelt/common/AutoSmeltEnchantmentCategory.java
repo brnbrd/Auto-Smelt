f8db51ee4c0e07508a71b48c8af24d1fffee5275
onVoid <onvoidmc@gmail.com>
2022-04-29 16:38:01 -0700

    Add support for lots of paxels, bump version, add workflow

--- src/main/java/net/onvoid/autosmelt/common/AutoSmeltEnchantmentCategory.java
+++ src/main/java/net/onvoid/autosmelt/common/AutoSmeltEnchantmentCategory.java
@@ -1,5 +1,7 @@
 package net.onvoid.autosmelt.common;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.ShovelItem;
@@ -8,5 +10,11 @@ import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class AutoSmeltEnchantmentCategory {
     public static final EnchantmentCategory SMELTERS = EnchantmentCategory.create("smelters", item ->
-            item instanceof PickaxeItem || item instanceof ShovelItem || item instanceof AxeItem || item instanceof SwordItem);
+        item instanceof PickaxeItem ||
+        item instanceof ShovelItem ||
+        item instanceof AxeItem ||
+        item instanceof SwordItem ||
+        ItemTags.CLUSTER_MAX_HARVESTABLES.contains(item) ||
+        ItemTags.createOptional(new ResourceLocation("forge", "tools/paxels")).contains(item)
+    );
 }
